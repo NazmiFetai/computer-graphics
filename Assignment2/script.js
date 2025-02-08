03f7e71386fc7ecb9c6661f42435edced6ba1568
NazmiFetai <nf30090@seeu.edu.mk>
2025-02-08 19:25:03 +0100

    Skypack solution

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,7 +1,7 @@
 // Import dependencies from npm-installed modules.
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.152.0/build/three.module.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.152.0/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.152.0/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "https://cdn.skypack.dev/three@0.173.0";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.173.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn/skypack.dev/three@0.173.0/examples/jsm/loaders/GLTFLoader.js";
 
 // Scene, Camera, and Renderer Setup
 const scene = new THREE.Scene();
