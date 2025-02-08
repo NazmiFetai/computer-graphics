1c53216feaf4f3c4ad9c01b338352795246f14ee
NazmiFetai <nf30090@seeu.edu.mk>
2025-02-08 19:20:46 +0100

    Changed imports

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,7 +1,7 @@
 // Import dependencies from npm-installed modules.
-import * as THREE from "/node_modules/three";
-import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "/node_modules/three/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.152.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.152.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.152.0/examples/jsm/loaders/GLTFLoader.js";
 
 // Scene, Camera, and Renderer Setup
 const scene = new THREE.Scene();
